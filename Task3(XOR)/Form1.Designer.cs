57246a16fc9e86626198a09f2204b6f62d659a9b
Sveta1528 <degtyareva1506@mail.ru>
2017-04-24 16:12:36 +0300

    Create multilayer perception for XOR

---
+++ Task3(XOR)/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Default = new MetroFramework.Controls.MetroRadioButton();
+            this.Selection = new MetroFramework.Controls.MetroRadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.layers = new System.Windows.Forms.ComboBox();
+            this.Learn = new System.Windows.Forms.Button();
+            this.Recognize = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.Speed = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Z = new System.Windows.Forms.TextBox();
+            this.Y = new System.Windows.Forms.TextBox();
+            this.X = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.TextBox();
+            this.Logs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Default
+            // 
+            this.Default.AutoSize = true;
+            this.Default.Checked = true;
+            this.Default.Location = new System.Drawing.Point(23, 22);
+            this.Default.Name = "Default";
+            this.Default.Size = new System.Drawing.Size(108, 15);
+            this.Default.TabIndex = 0;
+            this.Default.TabStop = true;
+            this.Default.Text = "По умолчанию";
+            this.Default.UseSelectable = true;
+            this.Default.CheckedChanged += new System.EventHandler(this.metroRadioButton1_CheckedChanged);
+            // 
+            // Selection
+            // 
+            this.Selection.AutoSize = true;
+            this.Selection.Location = new System.Drawing.Point(23, 43);
+            this.Selection.Name = "Selection";
+            this.Selection.Size = new System.Drawing.Size(129, 15);
+            this.Selection.TabIndex = 2;
+            this.Selection.Text = "Выбор параметров";
+            this.Selection.UseSelectable = true;
+            this.Selection.CheckedChanged += new System.EventHandler(this.metroRadioButton2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Количество слоев";
+            // 
+            // layers
+            // 
+            this.layers.Enabled = false;
+            this.layers.FormattingEnabled = true;
+            this.layers.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.layers.Location = new System.Drawing.Point(148, 67);
+            this.layers.Name = "layers";
+            this.layers.Size = new System.Drawing.Size(44, 21);
+            this.layers.TabIndex = 3;
+            this.layers.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Learn
+            // 
+            this.Learn.Location = new System.Drawing.Point(214, 27);
+            this.Learn.Name = "Learn";
+            this.Learn.Size = new System.Drawing.Size(153, 31);
+            this.Learn.TabIndex = 19;
+            this.Learn.Text = "Обучить";
+            this.Learn.UseVisualStyleBackColor = true;
+            this.Learn.Click += new System.EventHandler(this.Learn_Click);
+            // 
+            // Recognize
+            // 
+            this.Recognize.Location = new System.Drawing.Point(214, 99);
+            this.Recognize.Name = "Recognize";
+            this.Recognize.Size = new System.Drawing.Size(153, 31);
+            this.Recognize.TabIndex = 20;
+            this.Recognize.Text = "Распознать";
+            this.Recognize.UseVisualStyleBackColor = true;
+            this.Recognize.Click += new System.EventHandler(this.Recognize_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            // 
+            // Speed
+            // 
+            this.Speed.Location = new System.Drawing.Point(133, 158);
+            this.Speed.Name = "Speed";
+            this.Speed.Size = new System.Drawing.Size(44, 20);
+            this.Speed.TabIndex = 24;
+            this.Speed.Text = "0,5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Скорость обучения:";
+            // 
+            // Z
+            // 
+            this.Z.Location = new System.Drawing.Point(323, 73);
+            this.Z.Name = "Z";
+            this.Z.Size = new System.Drawing.Size(44, 20);
+            this.Z.TabIndex = 26;
+            this.Z.Text = "0";
+            // 
+            // Y
+            // 
+            this.Y.Location = new System.Drawing.Point(273, 73);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(44, 20);
+            this.Y.TabIndex = 27;
+            this.Y.Text = "0";
+            // 
+            // X
+            // 
+            this.X.Location = new System.Drawing.Point(214, 73);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(44, 20);
+            this.X.TabIndex = 28;
+            this.X.Text = "0";
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(214, 138);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(153, 20);
+            this.Result.TabIndex = 29;
+            // 
+            // Logs
+            // 
+            this.Logs.Location = new System.Drawing.Point(23, 217);
+            this.Logs.Multiline = true;
+            this.Logs.Name = "Logs";
+            this.Logs.Size = new System.Drawing.Size(344, 141);
+            this.Logs.TabIndex = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 394);
+            this.Controls.Add(this.Logs);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.Z);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Speed);
+            this.Controls.Add(this.Recognize);
+            this.Controls.Add(this.Learn);
+            this.Controls.Add(this.layers);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Selection);
+            this.Controls.Add(this.Default);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroRadioButton Default;
+        private MetroFramework.Controls.MetroRadioButton Selection;
+        private System.Windows.Forms.ComboBox layers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Learn;
+        private System.Windows.Forms.Button Recognize;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.TextBox Speed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Z;
+        private System.Windows.Forms.TextBox Y;
+        private System.Windows.Forms.TextBox X;
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.TextBox Logs;
+    }
+}
+
---
+++ Task3(XOR)/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : MetroForm
+    {
+        private NeuralNetwork net;
+        // Максимальное количество повторений обучения
+        const long num_iter = 5000000;
+        // Минимальная допустимая средняя квадратичная ошибка
+        const double Thresh = 0.000001;
+
+        public Form1()
+        {
+            InitializeComponent();
+            layers.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+        }
+
+        void DeleteLayersTextBox()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var control = this.Controls["layer" + (i + 1).ToString()];
+                if (Controls.Contains(control))
+                {
+                    Controls.Remove(control);
+                    Controls.Remove(this.Controls["layerlabel" + (i + 1).ToString()]);
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DeleteLayersTextBox();
+
+            var count = Int32.Parse(layers.SelectedItem.ToString());
+            int lx = 30, ly = 103, tbx = 55, tby = 98;
+            int k = 0;
+            for (int i = 0; i < count; i += 2)
+            {
+                int newlx = lx,
+                    newly = ly + k * 25,
+                    newtbx = tbx,
+                    newtby = tby + k * 25;
+                k++;
+                    this.Controls.Add(new Label()
+                    {
+                        Name = "layerlabel" + k.ToString(),
+                        Text = k.ToString() + ":",
+                        Location = new Point(newlx, newly),
+                        Size = new Size(23, 13)
+                    });
+                    this.Controls.Add(new TextBox()
+                    {
+                        Name = "layer" + k.ToString(),
+                        Location = new Point(newtbx, newtby),
+                        Size = new Size(46, 20)
+                    });
+            }
+            
+            int j = 0;
+            for (int i = 1; i < count; i += 2)
+            {
+                int newlx = lx+79,
+                    newly = ly + j * 25,
+                    newtbx = tbx+79,
+                newtby = tby + j * 25;
+                j++; k++;
+                    this.Controls.Add(new Label()
+                    {
+                        Name = "layerlabel" + k.ToString(),
+                        Text = k.ToString() + ":",
+                        Location = new Point(newlx, newly),
+                        Size = new Size(23, 13)
+                    });
+                    this.Controls.Add(new TextBox()
+                    {
+                        Name = "layer" + k.ToString(),
+                        Location = new Point(newtbx, newtby),
+                        Size = new Size(46, 20)
+                    });
+                
+            }
+            
+            
+        }
+
+        private void metroRadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            layers.Enabled = true;
+        }
+
+        private void metroRadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            layers.Enabled = false;
+            DeleteLayersTextBox();
+        }
+
+
+
+        private int[] GetCountsNeuron()
+        {
+            int count_input = data[0].Length;
+            int count_output = answer[0].Length;
+            if (Default.Checked)
+                return new[] { count_input, count_input, count_output };
+            else
+            {
+                int count_layers = Int32.Parse(layers.Text);
+                int n = 2 + count_layers;
+                var result = new int[n];
+                result[0] = count_input;
+                result[n - 1] = count_output;
+                for (int i = 1; i < n - 1; i++)
+                {
+                    var control = this.Controls["layer" + i.ToString()];
+                    result[i] = Int32.Parse(control.Text);
+                }
+                return result;
+
+            }
+
+        }
+
+        private void Learn_Click(object sender, EventArgs e)
+        {
+            int[] neuronsArr = GetCountsNeuron();
+            double speed = double.Parse(Speed.Text);
+            net = new NeuralNetwork(neuronsArr.Length, neuronsArr, speed, 0.1);
+            Logs.Text = "";
+            backgroundWorker.ReportProgress(10);
+            // Начинаем обучение сети
+            long i;
+            var sb = new StringBuilder();
+            for (i = 0; i < num_iter; i++)
+            {
+
+                // Запускаем обучение
+                net.BackPropogate(data[i % 8], answer[i % 8]);
+
+                // Проверяем среднюю кважротичную ошибку
+                var error = net.mse(answer[i % 8]);
+                if (error < Thresh)
+                {
+
+                    Logs.Text += "Сеть обучена. Выполнено " + i + " повтореий" + Environment.NewLine;
+                    Logs.Text += "Средняя квадратичная ошибка:  " + error + Environment.NewLine;
+                    break;
+                }
+
+
+                // Выводим статус каждые 10% прогресса
+                if (i % (num_iter / 10) == 0)
+                {
+                    error = net.mse(answer[i % 8]);
+                    Logs.Text += "Средняя квадратичная ошибка:  " + error + Environment.NewLine;
+                    Logs.Text += "... Идет обучение..." + Environment.NewLine;
+                }
+
+            }
+
+
+            // Если цикл дошел до предела, вместо того, чтобы средняя квадротичная ошибка стала приемлимой
+            if (i == num_iter)
+            {
+                Logs.Text += "Истекло количество повторений" + Environment.NewLine;
+                Logs.Text += "Средняя квадратичная ошибка:  " + net.mse(answer[i % 8]) + Environment.NewLine;
+            }
+        }
+
+        // Данные для обучения функции XOR
+        //double [] data  = {0,0,0,0};
+        double [][] data  = {
+        new double[] {0, 0, 0},
+        new double[] {0, 0, 1},
+        new double[] {0, 1, 0},
+        new double[] {0, 1, 1},
+        new double[] {1, 0, 0},
+        new double[] {1, 0, 1},
+        new double[] {1, 1, 0},
+        new double[] { 1, 1, 1}
+	};
+
+        private double[][] answer =
+        {
+            new double[] {0},
+            new double[] {1},
+            new double[] {1},
+            new double[] {0},
+            new double[] {1},
+            new double[] {0},
+            new double[] {0},
+            new double[] {1},
+        };
+
+        private void Recognize_Click(object sender, EventArgs e)
+        {
+            Result.Text = "";
+            net.FeedForwards(new []{double.Parse(X.Text), double.Parse(Y.Text) , double.Parse(Z.Text) });
+            foreach (var variable in net.Out())
+                Result.Text += variable.ToString() + "  ";
+                
+
+        }
+    }
+}
